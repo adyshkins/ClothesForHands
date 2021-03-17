5a8628d698858c3923568c22bfb6b8714e1e27b4
adyshkiss@outlook.com <adyshkiss@outlook.com>
2021-03-17 15:44:35 +0300

    Добавьте файлы проекта.

---
+++ ClothesForHands/EF/AppData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothesForHands.EF
+{
+    public class AppData
+    {
+        // Создание объекта Context для обращения к модели
+        public static Entities Context { get; } = new Entities();
+    }
+}
