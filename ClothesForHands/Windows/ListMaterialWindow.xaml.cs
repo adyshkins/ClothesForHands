a8ea99694559baba981940f5a5bb7c3fa62c68cb
adyshkiss@outlook.com <adyshkiss@outlook.com>
2021-03-18 12:46:13 +0300

    18/03

--- ClothesForHands/Windows/ListMaterialWindow.xaml.cs
+++ ClothesForHands/Windows/ListMaterialWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using static ClothesForHands.EF.AppData;
+using ClothesForHands.EF;
 
 namespace ClothesForHands
 {
@@ -21,10 +22,64 @@ namespace ClothesForHands
     /// </summary>
     public partial class ListMaterialWindow : Window
     {
+        List<Material> materialsList = new List<Material>();
+        int numberPege = 0;
+
         public ListMaterialWindow()
         {
             InitializeComponent();
-            lvMaterialList.ItemsSource = Context.Material.ToList();
+            Filter();
+
+        }
+
+        private void Filter()
+        {
+            materialsList = Context.Material.ToList();
+
+            materialsList = materialsList.OrderBy(i => i.ID).
+                Skip(numberPege * 15).Take(15).ToList();
+
+            lvMaterialList.ItemsSource = materialsList;
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (numberPege > 0)
+            {
+                numberPege--;
+            }
+            btnGo1.Content = (numberPege + 1).ToString();
+            btnGo2.Content = (numberPege + 2).ToString();
+            btnGo3.Content = (numberPege + 3).ToString();
+            Filter();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (materialsList.Count >= 15)
+            {
+                numberPege++;
+                
+                btnGo1.Content = (numberPege + 1).ToString();
+                btnGo2.Content = (numberPege + 2).ToString();
+                btnGo3.Content = (numberPege + 3).ToString();
+
+                int countPage = (materialsList.Count / 15) + 1;
+
+                if (Convert.ToInt32(btnGo2.Content) > countPage)
+                {
+                    btnGo2.Visibility = Visibility.Collapsed;
+                }
+
+                if (Convert.ToInt32(btnGo3.Content) > countPage)
+                {
+                    btnGo3.Visibility = Visibility.Collapsed;
+                }
+
+            }
+            
+            Filter();
+            //MessageBox.Show(numberPege.ToString());
         }
     }
 }
